e10ce5bcfeec77c6353a928fc455e07b7afbe4f0
MichaelRazo <michaelrazo23@gmail.com>
2023-07-11 23:31:48 -0500

    Se agrego carpeta Part1

---
+++ Part1/js/main.js
@@ -0,0 +1,12 @@
+//Ejercicio Parte1
+
+function businessHours(dayNumber,hourNumber){
+if ((dayNumber>=1) && (dayNumber<=5)&&
+(hourNumber>=9) && (hourNumber<=18)
+     ){
+return true;
+}else {
+    return false;
+}
+
+}
